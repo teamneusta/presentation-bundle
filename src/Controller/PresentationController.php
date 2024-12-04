b6eed720fd8524b2c6034aaac2c95da83df2bb31
Luka Dschaak <l.dschaak@neusta.de>
2024-12-04 13:32:40 +0100

    [update] allow php version 8.3

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -4,6 +4,7 @@ namespace Neusta\Pimcore\PresentationBundle\Controller;
 
 use Neusta\Pimcore\PresentationBundle\Renderer\PresentationRenderer;
 use Pimcore\Controller\FrontendController;
+use Pimcore\Model\Document\PageSnippet;
 use Symfony\Component\HttpFoundation\Response;
 
 class PresentationController extends FrontendController
@@ -14,8 +15,13 @@ class PresentationController extends FrontendController
 
     public function presentationAction(): Response
     {
+        $slidesMarkup = '';
+        if ($this->document instanceof PageSnippet) {
+            $slidesMarkup = $this->presentationRenderer->renderPresentation($this->document);
+        }
+
         return $this->render('@NeustaPimcorePresentation/Presentation/presentation.html.twig', [
-            'slidesMarkup' => $this->presentationRenderer->renderPresentation($this->document),
+            'slidesMarkup' => $slidesMarkup,
         ]);
     }
 }
