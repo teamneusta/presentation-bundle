424525d24aaf65f456d5b46466ffa29dc85a5182
Luka Dschaak <l.dschaak@neusta.de>
2024-12-05 08:59:21 +0100

    [update] allow php version 8.3 (#31)

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -4,6 +4,7 @@ namespace Neusta\Pimcore\PresentationBundle\Controller;
 
 use Neusta\Pimcore\PresentationBundle\Renderer\PresentationRenderer;
 use Pimcore\Controller\FrontendController;
+use Pimcore\Model\Document\PageSnippet;
 use Symfony\Component\HttpFoundation\Response;
 
 class PresentationController extends FrontendController
@@ -12,10 +13,18 @@ class PresentationController extends FrontendController
     {
     }
 
+    /**
+     * @throws \RuntimeException
+     */
     public function presentationAction(): Response
     {
+        if (!$this->document instanceof PageSnippet) {
+            throw new \RuntimeException('Presentation slides can only be used with PageSnippets');
+        }
+        $slidesMarkup = $this->presentationRenderer->renderPresentation($this->document);
+
         return $this->render('@NeustaPimcorePresentation/Presentation/presentation.html.twig', [
-            'slidesMarkup' => $this->presentationRenderer->renderPresentation($this->document),
+            'slidesMarkup' => $slidesMarkup,
         ]);
     }
 }
